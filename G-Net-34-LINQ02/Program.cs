d24f641d8115105764ef239d721960405c673e03
Kareem <krda4969@gmail.com>
2026-04-10 14:19:09 +0200

    Q02

--- G-Net-34-LINQ02/Program.cs
+++ G-Net-34-LINQ02/Program.cs
@@ -12,7 +12,12 @@ namespace G_Net_34_LINQ02
             //    .Take(3);
             //Helper.PrintWithNewLine("Top 3 Product For Price", query);
             #endregion
-
+            #region Q02
+            //show page 2 of products, with page size = 5
+            //var query=ProductList
+            //    .Skip(5).Take(5);
+            //Helper.PrintWithNewLine("6 to 10 Products", query);
+            #endregion
 
 
         }
