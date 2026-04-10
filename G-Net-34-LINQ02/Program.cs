a0d796eaa4092f5e87917210cfeea474e6d99d6c
Kareem <krda4969@gmail.com>
2026-04-10 14:17:30 +0200

    Q01

--- G-Net-34-LINQ02/Program.cs
+++ G-Net-34-LINQ02/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace G_Net_34_LINQ02
+﻿using static G_Net_34_LINQ02.DataSources.Source;
+namespace G_Net_34_LINQ02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            ////Get top 3 most expensive products
+            //var query = ProductList
+            //    .OrderByDescending(p => p.UnitPrice)
+            //    .Take(3);
+            //Helper.PrintWithNewLine("Top 3 Product For Price", query);
+            #endregion
+
+
+
         }
     }
 }
